72c3bd58c05235fc6fc9e45ef593ff6f90b3ac4b
yIuriy <contatoiuri20@gmail.com>
2025-09-11 09:24:23 -0300

    init of login roue

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -3,4 +3,22 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@test.com" && loginDTO.Password == "123456")
+    {
+        return Results.Ok("Login feito com sucesso.");
+    }
+    else
+        return Results.Ok("Login não autorizado .");
+}
+);
+
 app.Run();
+
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
