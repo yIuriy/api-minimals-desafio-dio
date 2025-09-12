4a04d1be7c046abac4aa273288a049d57d2cb5a5
yIuriy <contatoiuri20@gmail.com>
2025-09-12 09:32:23 -0300

    crete the test folder

--- minimal-api/obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
+++ minimal-api/obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a8c37eec2bf66fba15df81fd5a873acd177935")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ebb6aec6456aa39ec26c187ddbea7f28c6c267")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
